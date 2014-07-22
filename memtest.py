397460213f03ed3f0302722e92feb8c3e1f0a676
Matthew Gidden <matthew.gidden@gmail.com>
2014-07-22 09:50:25 -0500

    updated memtest

--- memtest.py
+++ memtest.py
@@ -3,9 +3,10 @@ from __future__ import print_function
 import sys
 import glob
 import gc
+import resource
 
-N = 10**7
-Ndiv10 = N/10
+N = 10**4
+freq = N/100
 
 buildlib = glob.glob('build/lib.*')
 sys.path.insert(0, buildlib[0])
@@ -16,8 +17,8 @@ for n in xrange(N):
     #gc.collect()
     h = Hollow()
     del h
-    if n%Ndiv10 == 0:
-        print(n/Ndiv10, resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+    if n%freq == 0:
+        print(n/freq, resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         sys.stdout.flush()
 
 print()
