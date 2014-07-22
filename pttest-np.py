3bac79c17a723c639e93044d33c1b2a77642cb9c
Anthony Scopatz <scopatz@gmail.com>
2014-07-21 22:22:31 -0500

    some tests

---
+++ pttest-np.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python 
+from __future__ import print_function
+import os
+import gc
+import sys
+
+import numpy as np
+import tables as tb
+
+N = 10**5
+Ndiv10 = N/10
+
+_N_CAPS_MAX = 10
+
+exarc = np.dtype([
+    ("instid", ('str', 16)), # 16 bytes for uuid
+    ("id", np.int64),
+    ("uid", np.int64),
+    ("ucaps", (np.float64, _N_CAPS_MAX),), # array of size N_CAPS_MAX
+    ("vid", np.int64),
+    ("vcaps", (np.float64, _N_CAPS_MAX),), # array of size N_CAPS_MAX
+    ("pref", np.float64),
+    ])
+
+data = np.empty(N, dtype=exarc)
+
+with tb.open_file('x.h5', 'w') as f:
+    tab = f.create_table('/', 'tab', exarc, chunkshape=(N/1000,))
+    for n, dat in enumerate(data):
+        gc.collect()
+        tab.append(np.array([dat]))
+        tab.flush()
+        if n%Ndiv10 == 0:
+            print(n/Ndiv10, end=" ")
+            sys.stdout.flush()
+print()
--- readme.rst
+++ readme.rst
@@ -14,3 +14,22 @@ Play around with the ``N`` value in memtest and un/comment the gc.collect() call
 inside of the loop.  If you are running h/top you'll see that the memory 
 resources are cleaned up correctly.
 
+Test 2: pytables row
+--------------------
+This test definitely mem leaks. This uses the table row object and the 
+memory leak seems to be fairly fast.  Run this test with::
+
+    $ ./pttest.py
+
+Test 3: pytables append
+------------------------
+This test uses the table append() method rather than the row object. 
+It also directly appends numpy arrays.  This may have a minor memory 
+leak but it is much slower.  Run this test with::
+
+    $ pttest-np.py
+
+Conclusion
+----------
+The row attr should probably be avoided, not just because it is slow!
+
